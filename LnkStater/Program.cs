111ce7a50a53abc30f3e2096c79bb11ad6a1fab3
HarmonyDock <panpdg@dingtalk.com>
2026-04-18 20:09:41 +0800

    Add files via upload

---
+++ LnkStater/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.IO;
+string exeNameWithoutExt = Path.ChangeExtension(Process.GetCurrentProcess().MainModule.FileName, ".lnk");
+var startInfo = new ProcessStartInfo
+{
+    FileName = exeNameWithoutExt,
+    UseShellExecute = true
+};
+if (File.Exists(exeNameWithoutExt))
+{
+    Process.Start(startInfo);
+}
+Console.ReadKey();
